3f259074bb12a6c6b199e3879b1b7406c1d3dc2c
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-07-27 22:40:14 -0700

    first navigator

---
+++ app/containers/Splash/SplashContainer.js
@@ -0,0 +1,11 @@
+import React, { PropTypes, Component } from 'react';
+import { Navigator } from 'react-native';
+import { Splash } from '~/components'
+
+export default class SplashContainer extends Component {
+  render() {
+    return (
+      <Splash />
+    );
+  }
+}
